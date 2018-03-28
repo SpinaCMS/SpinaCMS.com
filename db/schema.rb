6fa0ff3db9205a242b4a7267d4fd7da623ef4400
Bram Jetten <mail@bramjetten.nl>
2018-03-28 19:22:12 +0200

    Lets update to Rails 5.2.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906105143) do
+ActiveRecord::Schema.define(version: 2018_03_28_170842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
     t.string "name"
@@ -45,6 +66,26 @@ ActiveRecord::Schema.define(version: 20170906105143) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_image_collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_image_collections_images", id: :serial, force: :cascade do |t|
+    t.integer "image_collection_id"
+    t.integer "image_id"
+    t.integer "position"
+    t.index ["image_collection_id"], name: "index_spina_image_collections_images_on_image_collection_id"
+    t.index ["image_id"], name: "index_spina_image_collections_images_on_image_id"
+  end
+
+  create_table "spina_images", force: :cascade do |t|
+    t.integer "media_folder_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["media_folder_id"], name: "index_spina_images_on_media_folder_id"
   end
 
   create_table "spina_layout_parts", id: :serial, force: :cascade do |t|
