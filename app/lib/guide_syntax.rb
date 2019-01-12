e38c5224c8814ed06e4984f5e9c504f9d77f2e51
Bram Jetten <mail@bramjetten.nl>
2019-01-12 17:06:39 +0100

    Syntax

--- app/lib/guide_syntax.rb
+++ app/lib/guide_syntax.rb
@@ -3,7 +3,7 @@ class GuideSyntax
     
     html ||= ""
 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, disable_indented_code_blocks: true, lax_spacing: true)
     html = markdown.render(html)
 
     html = html.gsub(/\[youtube\s+(.*)\]/, '<div class="screencast-player" data-type="youtube" data-video-id="\1"></div>')
